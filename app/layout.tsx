66dbb84d739a8f5f58a485cfb618f72eb6b95964
Bonchon2023 <141843280+Bonchon2023@users.noreply.github.com>
2026-02-17 22:26:09 +0700

    Update icon in metadata with base64 SVG

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "SkillFrameX", // เปลี่ยนชื่อที่แสดงบน Tab
   description: "SkillFrameX Online Learning Platform", // คำอธิบายเว็บไซต์สำหรับการทำ SEO
   icons: {
-    icon: SquareCode, // 
+    icon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNxdWFyZS1jb2RlLWljb24gbHVjaWRlLXNxdWFyZS1jb2RlIj48cGF0aCBkPSJtMTAgOS0zIDMgMyAzIi8+PHBhdGggZD0ibTE0IDE1IDMtMy0zLTMiLz48cmVjdCB4PSIzIiB5PSIzIiB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHJ4PSIyIi8+PC9zdmc+",
   },
 };
 
