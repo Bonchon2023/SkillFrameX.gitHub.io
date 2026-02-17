c2f39607f1659721d6ecb9a672789822a1788b71
Bonchon2023 <141843280+Bonchon2023@users.noreply.github.com>
2026-02-17 22:16:16 +0700

    Change icon reference in RootLayout metadata
    Updated the icon reference in metadata to use 'SquareCode'.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
-
+import SquareCode  from "lucide-react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "SkillFrameX", // เปลี่ยนชื่อที่แสดงบน Tab
   description: "SkillFrameX Online Learning Platform", // คำอธิบายเว็บไซต์สำหรับการทำ SEO
   icons: {
-    icon: "/favicon.ico", // ระบุตำแหน่งไฟล์ไอคอน (ปกติ Next.js จะหาใน folder app/ หรือ public/ อัตโนมัติ แต่ระบุไว้เพื่อความชัวร์)
+    icon: "SquareCode", // 
   },
 };
 
